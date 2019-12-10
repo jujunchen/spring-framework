5ced2fb7a65c857be952313fe1a64c3daaaa8f92
chenjujun <1913210361@qq.com>
2019-12-10 22:27:59 +0800

    spring-core @Nullable注释

--- spring-core/src/main/java/org/springframework/lang/Nullable.java
+++ spring-core/src/main/java/org/springframework/lang/Nullable.java
@@ -39,6 +39,15 @@ import javax.annotation.meta.When;
  * <p>Can be used in association with {@code @NonNullApi} or {@code @NonNullFields} to
  * override the default non-nullable semantic to nullable.
  *
+ * <p>
+ *     一个通用的Spring注释，用于声明被注释的元素在某些情况下可以为null。
+ * <p>
+ * 		利用JSR-305元注释在支持JSR-305的通用工具上指示Java中的可空性，并由Kotlin用来推断Spring API的可空性。
+ * <p>
+ *	 	应该在参数，返回值和字段级别上使用。重写方法应重复父@Nullable批注，除非它们的行为不同。
+ * 可以与@NonNullApi或@NonNullFields结合使用，以将默认的不可为null的语义覆盖为可为null。
+ * </p>
+ *
  * @author Sebastien Deleuze
  * @author Juergen Hoeller
  * @since 5.0
